f8d9c2cdb34ea478785330e4856f99f1fc5ec1ec
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-13 13:23:19 -0400

    Update test.py

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -3,7 +3,7 @@ from sklearn.linear_model import LinearRegression
 def linear_regression(X_train, y_train):
   
   lr = LinearRegression().fit(X_train, y_train)
-  y_pred = lr.predict(X_train)
+  res = {'coef': lr.coef_, 'intercept': lr.intercept, 'pred': lr.predict(X_train)}
   
   with open("res.json",'w') as f:
-        f.write(json.dumps(y_pred)) # Write predictions to file as this will serve as output artifact
+        f.write(json.dumps(res)) # Write predictions to file as this will serve as output artifact
