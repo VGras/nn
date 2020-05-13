f05a924401ca1a6e0d202686147289132c50afb2
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-13 14:46:45 -0400

    Update test.py

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -6,7 +6,7 @@ def linear_regression():#(X_train, y_train):
   #lr = LinearRegression().fit(X_train, y_train)
   #res = {'coef': lr.coef_.tolist(), 'intercept': lr.intercept_, 'pred': lr.predict(X_train).tolist()}
   #res = {'X-train': X_train, 'y-train': y_train}
-  res = {'test: 'test'}
+  res = {'test': 'test'}
   res['schema'] = "test"
   
   with open("res.json",'w') as f:
