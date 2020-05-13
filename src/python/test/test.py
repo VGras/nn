b80c70c2969ae68d126d070f1193e5dad146183a
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-13 13:54:19 -0400

    Update test.py

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -5,6 +5,7 @@ def linear_regression(X_train, y_train):
   
   #lr = LinearRegression().fit(X_train, y_train)
   #res = {'coef': lr.coef_.tolist(), 'intercept': lr.intercept_, 'pred': lr.predict(X_train).tolist()}
-  res = {'a': 1, 'b': 2}
+  res = {'X-train': X_train, 'y-train': y_train}
+  
   with open("res.json",'w') as f:
         f.write(json.dumps(res)) # Write predictions to file as this will serve as output artifact
