054fa43ff5a43e087913b7391758a031a86e3cf4
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-13 13:51:58 -0400

    Update test.py

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -1,10 +1,10 @@
 import json
-from sklearn.linear_model import LinearRegression
+#from sklearn.linear_model import LinearRegression
 
 def linear_regression(X_train, y_train):
   
-  lr = LinearRegression().fit(X_train, y_train)
-  res = {'coef': lr.coef_.tolist(), 'intercept': lr.intercept_, 'pred': lr.predict(X_train).tolist()}
-  
+  #lr = LinearRegression().fit(X_train, y_train)
+  #res = {'coef': lr.coef_.tolist(), 'intercept': lr.intercept_, 'pred': lr.predict(X_train).tolist()}
+  res = {'a': 1, 'b': 2}
   with open("res.json",'w') as f:
         f.write(json.dumps(res)) # Write predictions to file as this will serve as output artifact
