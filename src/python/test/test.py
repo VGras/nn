0d61d4d3a9811029b0b420990009f2c71382dffd
Victoria Gras Andreu <49200243+VGras@users.noreply.github.com>
2020-05-13 14:04:07 -0400

    Update test.py

--- src/python/test/test.py
+++ src/python/test/test.py
@@ -1,11 +1,12 @@
 import json
 #from sklearn.linear_model import LinearRegression
 
-def linear_regression(X_train, y_train):
+def linear_regression():#(X_train, y_train):
   
   #lr = LinearRegression().fit(X_train, y_train)
   #res = {'coef': lr.coef_.tolist(), 'intercept': lr.intercept_, 'pred': lr.predict(X_train).tolist()}
-  res = {'X-train': X_train, 'y-train': y_train}
+  #res = {'X-train': X_train, 'y-train': y_train}
+  res = {'test: 'test'}
   res['schema'] = "test"
   
   with open("res.json",'w') as f:
